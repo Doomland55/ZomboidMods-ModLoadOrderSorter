0463227644ded10e01f25389bdeb48aea29c3e7f
Doomland55 <d-o-k-k@mail.ru>
2025-10-01 22:53:37 +0300

    Circular dependency exception handling: now the exception does not block sorting

--- Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/ModSelector/MLOS_sorting.lua
+++ Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/ModSelector/MLOS_sorting.lua
@@ -13,7 +13,7 @@
 local utils = require('OptionScreens/ModSelector/Refr_utils')
 
 local RULES_FILE = "sorting_rules.txt"
-local MOD_VERSION = "1.0.11"
+local MOD_VERSION = "1.0.12"
 
 local preorder = { ModManager = 1, ModManagerServer = 2, modoptions = 3 }
 local rawCategoryOrder = { "coreRequirement", "resource", "map", "vehicle", "tweaks", "code", "clothes", "ui", "other",	"translation", "undefined" }
@@ -300,13 +300,23 @@ local function initialSortMods(a, b)
 	return a.id:lower() < b.id:lower()
 end
 
+
+local function dependency_cycle_exception(visting_mods)
+	local visited = {}; for k, _ in pairs(visting_mods) do table.insert(visited, k) end
+	error("WARNING: Dependencyes Cycle Detected for " .. utils:getString(visited))
+end
+
+
 local function topological_sort(mods_list, mods_cache)
 	local sorted = {}
 	local visited = {}
 	local visiting = {}
 
 	local function visit(mod)
-		if visiting[mod.id] then error("Dependencyes Cycle Detected for " .. mod.id) end
+		if visiting[mod.id] then
+			pcall(dependency_cycle_exception, visiting)
+			return
+		end
 
 		if not visited[mod.id] then
 			mods_cache[mod.id].warnings = {}
