f39d29289cc4eaec7c34817f160dc058797da5e3
Doomland55 <d-o-k-k@mail.ru>
2025-12-20 17:14:36 +0300

    [MP_support] added working logic with backups

--- Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/MLOS_ServerSettingsScreen_overrides.lua
+++ Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/MLOS_ServerSettingsScreen_overrides.lua
@@ -10,10 +10,10 @@
 ---
 --- This mod has no dependencies
 ---
-require "ISUI/ISPanelJoypad"
 require "OptionScreens/ServerSettingsScreen"
 local utils = require('OptionScreens/ModSelector/Refr_utils')
 
+local rulesTexture = getTexture("media/ui/MLOS_From_Client.png")
 local FONT_HGT_SMALL = getTextManager():getFontHeight(UIFont.Small)
 local BUTTON_HGT = FONT_HGT_SMALL + 6
 local UI_BORDER_SPACING = 10
@@ -21,28 +21,10 @@ local JOYPAD_TEX_SIZE = 32
 local BUTTON_PADDING = JOYPAD_TEX_SIZE + UI_BORDER_SPACING*2
 
 
-
-MLOS_ServerSettingsScreen_overrides = {}
-
-
-function MLOS_ServerSettingsScreen_overrides.AddFromClientButton(chooseModsWindow)
-    -- local btnWidth = BUTTON_PADDING + getTextManager():MeasureStringX(UIFont.Small, getText("UI_btn_back"))
-    local btnWidth = BUTTON_PADDING + getTextManager():MeasureStringX(UIFont.Small, "Use client mods")
-	
-    chooseModsWindow.fromClientBtn = ISButton:new(chooseModsWindow.buttonMods:getRight() + UI_BORDER_SPACING , chooseModsWindow.buttonMods.y, btnWidth, BUTTON_HGT, "Use client mods", chooseModsWindow, MLOS_ServerSettingsScreen_overrides.OnFromClientButton)
-	chooseModsWindow.fromClientBtn:getRight()
-    chooseModsWindow.fromClientBtn:initialise()
-	chooseModsWindow.fromClientBtn:setAnchorLeft(true)
-	chooseModsWindow.fromClientBtn:setAnchorTop(false)
-	chooseModsWindow.fromClientBtn:setAnchorBottom(true)
-	chooseModsWindow.fromClientBtn.borderColor = {r=1, g=1, b=1, a=0.1}
-	chooseModsWindow:addChild(chooseModsWindow.fromClientBtn)
-
-    print("[MLOS] Called AddFromClientButton")
-end
-
-function MLOS_ServerSettingsScreen_overrides:OnFromClientButton()
-    print("[MLOS] Pressed OnFromClientButton")
+--================================================
+--    ChooseModsWindow add fromClient Button
+--================================================
+local onFromClientButton = function(self, ...)
     self.listbox:clear()
     local currentClientMods = getActivatedMods()
     for i=1, currentClientMods:size() do
@@ -50,201 +32,145 @@ function MLOS_ServerSettingsScreen_overrides:OnFromClientButton()
     end
 end
 
-
-function MLOS_ServerSettingsScreen_overrides.UpdatedOnNextButton(pageEdit)
-    print("[MLOS] Called UpdatedOnNextButton")
-    pageEdit.chooseModsWindow.buttonAccept:setTitle("puk")
-
-    local origOnButtonNext = pageEdit.chooseModsWindow.onButtonNext
-    if not origOnButtonNext then
-        print("[MLOS] origOnButtonNext not found")
-        return
-    end
-
-    local over = function(self)
-
-        -- local cur = self.settings:getServerOptions():getOptionByName("WorkshopItems"):getValue()
-        -- print("[MLOS] Curernt settings:", cur)
-
-        local function getWorkshopIdFromDir(dir) return dir:match("108600\\(%d+)\\") end
-
-        local finalData = {}
-        for _, item in ipairs(self.listbox.items) do
-            local dir = item.item.modInfo:getDir()
-            local wsId = getWorkshopIdFromDir(dir)
-            -- print("[MLOS] checking item: dir=", dir, " wsId=", wsId )
-
-            if wsId and not utils:contains(finalData, wsId) then
-                table.insert(finalData, wsId)
-            else
-                print("[MLOS] not found workshopId for", dir)
-            end
-        end
-
-        self.settings:getServerOptions():getOptionByName("WorkshopItems"):setValue(table.concat(finalData, ";"))
-
-        -- utils:tprint(finalData)
-        -- local new = self.settings:getServerOptions():getOptionByName("WorkshopItems"):getValue()
-        -- print("[MLOS] New settings:", new)
-        
-        -- self.settings:getServerOptions():getOptionByName("WorkshopItems"):setValue(table.concat(finalData, ";"))
-        -- origOnButtonNext(self)
-
-		-- for _, panel in ipairs(self.parent.pageEdit.customui) do
-		-- 	if panel.Type == "ServerSettingsScreenWorkshopPanel" then
-        --         -- panel:setSettings(self.settings)
-        --         -- panel.listbox:clear()
-        --         -- for _, wsId in ipairs(finalData) do
-        --         --     panel:addItemToList(wsId)
-        --         -- end
-
-		-- 		break
-		-- 	end
-		-- end
-
-        self:setVisible(false)
-
-        local activeMods = ActiveMods.getById("serversettings")
-        local modArray = activeMods:getMods()
-        modArray:clear()
-        for _, item in ipairs(self.listbox.items) do
-            modArray:add(item.item.modID)
-        end
-
-        self.parent.pageEdit.settings = self.settings
-        self.parent.pageEdit.settings:saveFiles()
-        self.parent.pageEdit:aboutToShow()
-        self.parent.pageEdit:setVisible(true, self.joyfocus)
-
-        for _, panel in ipairs(self.parent.pageEdit.customui) do
-            if panel.Type == "ServerSettingsScreenModsPanel" then
-                panel.listbox:clear()
-                for i = 0, modArray:size()-1 do
-                    panel:addModToList(modArray:get(i))
-                end
-            end
-
-            if panel.Type == "ServerSettingsScreenWorkshopPanel" then
-                print("MLOS ServerSettingsScreenWorkshopPanel do: ", panel.settings:getServerOptions():getOptionByName("WorkshopItems"):getValue())
-                utils:tprint(panel.listbox.items)
-                panel.listbox:clear()
-                for _, wsId in ipairs(finalData) do
-                    panel:addItemToList(wsId)
-                end
-                utils:tprint(panel.listbox.items)
-                print("MLOS ServerSettingsScreenWorkshopPanel posle: ", panel.settings:getServerOptions():getOptionByName("WorkshopItems"):getValue())
-			end
-        end
-
-        local reason = "ServerSettingsChange" .. "=" .. self.settings:getName()
-        if ActiveMods.requiresResetLua(activeMods) then
-            getCore():ResetLua("serversettings", reason)
-        end
-        
-    end
-
-    pageEdit.chooseModsWindow.buttonAccept:setOnClick(over, pageEdit.chooseModsWindow)
+local function addPageEditFromClientButton(self, ...)
+    self.fromClientBtn = ISButton:new(self.buttonMods:getRight() + UI_BORDER_SPACING , self.buttonMods.y, BUTTON_HGT, BUTTON_HGT, "", self, onFromClientButton)
+    self.fromClientBtn:setImage(rulesTexture)
+    self.fromClientBtn:setTextureRGBA(1, 1, 1, 0.9)
+    self.fromClientBtn:setTooltip("Use mods from client")
+	self.fromClientBtn:getRight()
+    self.fromClientBtn:initialise()
+	self.fromClientBtn:setAnchorLeft(true)
+	self.fromClientBtn:setAnchorTop(false)
+	self.fromClientBtn:setAnchorBottom(true)
+	self.fromClientBtn.borderColor = {r=1, g=1, b=1, a=0.1}
+	self:addChild(self.fromClientBtn)
 end
 
+--================================================
+--             PageEdit overrides
+--================================================
+local backupPrefix = "__backup__"
+
+local function createSettingsBackup(settings)
+    local newName = backupPrefix .. settings:getName()
+    settings:duplicateFiles(newName)
+end
+
+local function getSettingsByName(settingsName)
+    local serverManager = getServerSettingsManager()
+    serverManager:readAllSettings()
+    for i=1,serverManager:getSettingsCount() do
+		local settings = serverManager:getSettingsByIndex(i-1)
+        if settings:getName() == settingsName then
+            return settings
+        end
+	end
+end
+
+local function deleteSettingsBackup(settingsName)
+    if not utils:strContainsAny(settingsName, backupPrefix) then settingsName = backupPrefix .. settingsName end
+    local settingsToDelete = getSettingsByName(settingsName)
+    if settingsToDelete then settingsToDelete:deleteFiles() end
+end
+
+local function revertToSettingsBackup(settings)
+    if not settings then error("MLOS no settings to revert") end
+
+    local targetName = settings:getName()
+    local backupName = targetName
+    if not utils:strContainsAny(backupName, backupPrefix) then backupName = backupPrefix .. backupName end
+
+    local backupSettings = getSettingsByName(backupName)
+    if backupSettings then
+        settings:deleteFiles()
+        backupSettings:rename(targetName)
+    end
+end
+
+local function overrideOnButtonCancel(self, ...)
+    local origFunc = self.onButtonCancel
+    local override = function(self, ...)
+        revertToSettingsBackup(self.settings)
+        origFunc(self)
+    end
+    self.buttonCancel:setOnClick(override, self)
+end
+
+local function overrideOnButtonSave(self, ...)
+    local origFunc = self.onButtonSave
+    local override = function(self, ...)
+        deleteSettingsBackup(self.settings:getName())
+        origFunc(self)
+    end
+    self.buttonAccept:setOnClick(override, self)
+end
+
+--================================================
+--    ChooseModsWindow add fromClient Button
+--================================================
+local function getWorkshopId(modInfo)
+    local workshopId = modInfo:getWorkshopID()
+    if not workshopId or workshopId == "" then
+        local dir = modInfo:getDir()
+        return dir:match("108600\\(%d+)\\")
+    end
+end
+
+local function updateServerActiveMods(mods, settingsName)
+    local activeMods = ActiveMods.getById("serversettings")
+	local modArray = activeMods:getMods()
+	modArray:clear()
+	for _, item in ipairs(mods) do
+		modArray:add(item.item.modID)
+	end
+
+    if ActiveMods.requiresResetLua(activeMods) then
+        local reason = "ServerSettingsChange=" .. settingsName
+		getCore():ResetLua("serversettings", reason)
+        return true
+    else
+        return false
+	end
+end
+
+local function newChooseModsOnNextButton(self, ...)
+    local modIDs = {}
+    local workshopIDs = {}
+    for _, item in ipairs(self.listbox.items) do
+        table.insert(modIDs, item.item.modID)
+        local workshopId = getWorkshopId(item.item.modInfo)
+        if workshopId and workshopId ~= "" and not utils:contains(workshopIDs, workshopId) then
+            table.insert(workshopIDs, workshopId)
+        else
+            print("[MLOS] not found workshopId for", item.item.modInfo:getId())
+        end
+    end
+
+    createSettingsBackup(self.settings)
+    self.settings:getServerOptions():getOptionByName("Mods"):setValue(table.concat(modIDs, ";"))
+    self.settings:getServerOptions():getOptionByName("WorkshopItems"):setValue(table.concat(workshopIDs, ";"))
+    self.settings:saveFiles()
+
+    self:setVisible(false)
+    if not updateServerActiveMods(self.listbox.items, self.settings:getName()) then
+        self.parent.pageEdit.settings = self.settings
+        self.parent.pageEdit:aboutToShow()
+        self.parent.pageEdit:setVisible(true, self.joyfocus)
+    end
+end
+
+--================================================
+--    Apply ServerSettingsScreen Overrides
+--================================================
 
 local function applyOverrides()
     local pageEdit = ServerSettingsScreen.instance.pageEdit
-    MLOS_ServerSettingsScreen_overrides.AddFromClientButton(pageEdit.chooseModsWindow)
-    MLOS_ServerSettingsScreen_overrides.UpdatedOnNextButton(pageEdit)
+    overrideOnButtonCancel(pageEdit)
+    overrideOnButtonSave(pageEdit)
+
+    local chooseModsWindow = pageEdit.chooseModsWindow
+    addPageEditFromClientButton(chooseModsWindow)
+    chooseModsWindow.buttonAccept:setOnClick(newChooseModsOnNextButton, chooseModsWindow)
 end
 
 Events.OnMainMenuEnter.Add(applyOverrides)
-
--- local origCreate = sss.create
--- local origOnButtonNext = ServerSettingsScreen.instance.pageEdit.chooseModsWindow.onButtonNext
-
---================================================
---      ServerSettingsScreen Overrides
---================================================
--- function sss:create()
---     origCreate(self)
-
---     print("ENTER IN ServerSettingsScreen.create OVERRIDES [MLOS]")
-
---     local choseModWindow = self.pageEdit.chooseModsWindow
---     local origOnButtonNext = choseModWindow.onButtonNext
-
---     local function newOnButtonNext(self)
---         origOnButtonNext(self)
---         -- origOnButtonNext(SSS.pageEdit.ChooseModsWindow)
-
---         local finalData = {}
---         for _, item in ipairs(self.listbox.items) do
---             table.insert(finalData, item.item.modInfo:getWorkshopID())
---         end
---         print("MLOS mods workshopIDs:",  table.concat(finalData, ";"))
---     end
-
---     choseModWindow.onButtonNext = newOnButtonNext
---     -- self.settings:getServerOptions():getOptionByName("SteamWorkshop"):setValue(table.concat(finalData, ";"))
--- end
-
-
-
-
--- local ss = ServerSettingsScreen.instance.pageEdit
--- local listWindow = ServerSettingsScreen and ServerSettingsScreen.pageEdit and ServerSettingsScreen.pageEdit.chooseModsWindow
-
--- if listWindow and listWindow.onButtonNext then
---     local original = listWindow.onButtonNext
---     print("MLOS")
---     listWindow.onButtonNext = function(self, ...)
---         local results = {original(self, ...)}
-        
---         -- Ваш код после оригинальной функции
---         performCustomLogic(self, ...)
-        
---         return unpack(results)
---     end
--- else
---     print("MLOS ListWindow или OnButtonClick не найдены")
--- end
-
--- function performCustomLogic(self, button, ...)
---     print("MLOS Выполняется пользовательская логика")
---     -- Ваш код здесь
--- end
-
-
-
--- local function patchOnButtonClick()
---     -- Пытаемся найти объект сразу
---     local listWindow = ServerSettingsScreen and ServerSettingsScreen.instance and ServerSettingsScreen.instance.pageEdit and ServerSettingsScreen.instance.pageEdit.chooseModsWindow
-    
---     if listWindow and listWindow.onButtonNext then
---         -- Объект найден, патчим
---         local original = listWindow.onButtonNext
---         listWindow.onButtonNext = function(self, ...)
---             original(self, ...)
---             -- Ваш код
---             customButtonClickHandler(self, ...)
---         end
---         return true
---     end
-    
---     return false
--- end
-
--- -- Пытаемся патч сразу
--- if not patchOnButtonClick() then
---     -- Если не получилось, ждем через таймер
---     local attempts = 0
---     local maxAttempts = 50 -- 5 секунд при проверке каждые 100ms
-    
---     local timerId = Timer.SetInterval(function()
---         attempts = attempts + 1
---         if patchOnButtonClick() or attempts >= maxAttempts then
---             Timer.Kill(timerId)
---         end
---     end, 100)
--- end
-
--- function customButtonClickHandler(self, ...)
---     print("MLOS OnButtonClick:")
---     -- Ваша логика
--- end
