921656fc1ffa7112407805138c784854c3ca4b8e
Doomland55 <d-o-k-k@mail.ru>
2025-12-20 23:47:56 +0300

    [MP_support] added working layout fix and translations

--- Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/MLOS_ServerSettingsScreen_overrides.lua
+++ Contents/mods/ModLoadOrderSorter_b42/42.0/media/lua/client/OptionScreens/MLOS_ServerSettingsScreen_overrides.lua
@@ -1,6 +1,6 @@
 ---
----	This mod updates the behavior of automatic sorting, added to the Project Zomboid build 42.0. 
---- Adds own topological sorting algorithm for mods load order, 
+---	This mod updates the behavior of automatic sorting, added to the Project Zomboid build 42.0.
+--- Adds own topological sorting algorithm for mods load order,
 --- and adds support for sorting rules from the file sorting_rules.txt.
 ---
 --- Details about this mod:
@@ -25,21 +25,21 @@ local UI_BORDER_SPACING = 10
 local onFromClientButton = function(self, ...)
     self.listbox:clear()
     local currentClientMods = getActivatedMods()
-    for i=1, currentClientMods:size() do
-        self:addModToList(currentClientMods:get(i-1))
+    for i = 1, currentClientMods:size() do
+        self:addModToList(currentClientMods:get(i - 1))
     end
 end
 
 local function addPageEditFromClientButton(self, ...)
-    self.fromClientBtn = ISButton:new(self.buttonMods:getRight() + UI_BORDER_SPACING , self.buttonMods.y, BUTTON_HGT, BUTTON_HGT, "", self, onFromClientButton)
+    self.fromClientBtn = ISButton:new(self.buttonMods:getRight() + UI_BORDER_SPACING, self.buttonMods.y, BUTTON_HGT, BUTTON_HGT, "", self, onFromClientButton)
     self.fromClientBtn:setImage(rulesTexture)
     self.fromClientBtn:setTextureRGBA(1, 1, 1, 0.9)
-    self.fromClientBtn:setTooltip("Use mods from client")
-	self.fromClientBtn:setAnchorLeft(true)
-	self.fromClientBtn:setAnchorTop(false)
-	self.fromClientBtn:setAnchorBottom(true)
-	self.fromClientBtn.borderColor = {r=1, g=1, b=1, a=0.1}
-	self:addChild(self.fromClientBtn)
+    self.fromClientBtn:setTooltip(getText("UI_MLOS_FromClientBtn_Tooltip"))
+    self.fromClientBtn:setAnchorLeft(true)
+    self.fromClientBtn:setAnchorTop(false)
+    self.fromClientBtn:setAnchorBottom(true)
+    self.fromClientBtn.borderColor = { r = 1, g = 1, b = 1, a = 0.1 }
+    self:addChild(self.fromClientBtn)
 end
 
 --================================================
@@ -55,12 +55,12 @@ end
 local function getSettingsByName(settingsName)
     local serverManager = getServerSettingsManager()
     serverManager:readAllSettings()
-    for i=1,serverManager:getSettingsCount() do
-		local settings = serverManager:getSettingsByIndex(i-1)
+    for i = 1, serverManager:getSettingsCount() do
+        local settings = serverManager:getSettingsByIndex(i - 1)
         if settings:getName() == settingsName then
             return settings
         end
-	end
+    end
 end
 
 local function deleteSettingsBackup(settingsName)
@@ -101,19 +101,38 @@ local function overrideOnButtonSave(self, ...)
     self.buttonAccept:setOnClick(override, self)
 end
 
-local function overrideServerSettingsScreenPanelPrerender(self)
-    local options = (self.category == "INI") and self._instance.settings:getServerOptions() or self._instance.settings:getSandboxOptions()
-	local nonDefaultOptions = self._instance.nonDefaultOptions[self.category]
-	for _,settingName in ipairs(self.settingNames) do
-		local label = self.labels[settingName]
-		local control = self.controls[settingName]
-		if label and control then
-			label:setColor(1, 1, 1)
-			local option = nonDefaultOptions:getOptionByName(settingName)
-			if option and (option:getValue() ~= option:getDefaultValue()) then
-				label:setColor(1, 1, 0)
-			end
+local function newPageEditOnPanelChange(self)
+    local labelWidth = 0
+    self.currentPanel:setWidth(self.width - self.listbox:getRight() - UI_BORDER_SPACING * 2 - 1)
+    local panelWidth = self.currentPanel.width
+    local name
+
+    if self.currentPanel.settingNames then
+        for i = 1, #self.currentPanel.settingNames do
+            name = self.currentPanel.settingNames[i]
+            labelWidth = math.max(labelWidth,
+                self.currentPanel.labels[name].width + self.currentPanel.controls[name].width + UI_BORDER_SPACING)
         end
+        labelWidth = labelWidth + UI_BORDER_SPACING * 2
+
+        local labelLeft = math.max(UI_BORDER_SPACING, (panelWidth - labelWidth) * 0.5)
+        local controlLeft = math.min(panelWidth - UI_BORDER_SPACING, labelWidth + (panelWidth - labelWidth) * 0.5)
+
+        for i = 1, #self.currentPanel.settingNames do
+            name = self.currentPanel.settingNames[i]
+            self.currentPanel.labels[name]:setX(labelLeft)
+            self.currentPanel.controls[name]:setX(controlLeft - UI_BORDER_SPACING -
+            self.currentPanel.controls[name].width)
+            self.currentPanel.controls[name]:bringToTop()
+        end
+
+        for i = 1, #self.currentPanel.titles do
+            self.currentPanel.titles[i]:setX((panelWidth - self.currentPanel.titles[i].width) / 2)
+        end
+    end
+
+    for _, panel in ipairs(self.customui) do
+        panel:onResolutionChange()
     end
 end
 
@@ -121,7 +140,7 @@ local function fixLayout()
     local instance = ServerSettingsScreen.instance
 
     -- Fix server page layout if there are no mods that perform the same function.
-    local skipIfModExists = {"\\ClientModsToServer"}
+    local skipIfModExists = { "\\ClientModsToServer" }
     local activeMods = getActivatedMods()
     for _, modId in ipairs(skipIfModExists) do if activeMods:contains(modId) then return end end
 
@@ -142,7 +161,7 @@ local function fixLayout()
 
     -- resize chooseModsWindow
     local chooseModsWindow = instance.pageEdit.chooseModsWindow
-    local lsitboxW = targetW * 0.6
+    local lsitboxW = targetW * 0.5
     chooseModsWindow.listbox:setWidth(lsitboxW)
     chooseModsWindow.listbox:setHeight(targetH - FONT_HGT_LARGE - UI_BORDER_SPACING * 4 - BUTTON_HGT - 2)
     chooseModsWindow.listbox:setX(targetW * 0.5 - lsitboxW * 0.5)
@@ -151,9 +170,13 @@ local function fixLayout()
     chooseModsWindow.fromClientBtn:setX(chooseModsWindow.buttonMods:getRight() + UI_BORDER_SPACING)
 
     -- fix panels
-
-    -- fix settings boundingbox
-
+    for _, panel in ipairs(instance.pageEdit.customui) do
+        if panel.Type == "ServerSettingsScreenModsPanel" then
+            if panel.button then
+                panel.button:setVisible(false)
+            end
+        end
+    end
 end
 
 --================================================
@@ -169,22 +192,22 @@ end
 
 local function updateServerActiveMods(mods, settingsName)
     local activeMods = ActiveMods.getById("serversettings")
-	local modArray = activeMods:getMods()
-	modArray:clear()
-	for _, item in ipairs(mods) do
-		modArray:add(item.item.modID)
-	end
+    local modArray = activeMods:getMods()
+    modArray:clear()
+    for _, item in ipairs(mods) do
+        modArray:add(item.item.modID)
+    end
 
     if ActiveMods.requiresResetLua(activeMods) then
         local reason = "ServerSettingsChange=" .. settingsName
-		getCore():ResetLua("serversettings", reason)
+        getCore():ResetLua("serversettings", reason)
         return true
     else
         return false
-	end
+    end
 end
 
-local function newChooseModsOnNextButton(self, ...)
+local function newChooseModsWindowOnNextButton(self, ...)
     local modIDs = {}
     local workshopIDs = {}
     for _, item in ipairs(self.listbox.items) do
@@ -218,13 +241,13 @@ local function applyOverrides()
     local pageEdit = ServerSettingsScreen.instance.pageEdit
     overrideOnButtonCancel(pageEdit)
     overrideOnButtonSave(pageEdit)
+    pageEdit.onPanelChange = function(self, ...) newPageEditOnPanelChange(self) end
 
     local chooseModsWindow = pageEdit.chooseModsWindow
     addPageEditFromClientButton(chooseModsWindow)
-    chooseModsWindow.buttonAccept:setOnClick(newChooseModsOnNextButton, chooseModsWindow)
+    chooseModsWindow.buttonAccept:setOnClick(newChooseModsWindowOnNextButton, chooseModsWindow)
+    fixLayout()
 end
 
 Events.OnResolutionChange.Add(fixLayout)
 Events.OnMainMenuEnter.Add(applyOverrides)
-Events.OnMainMenuEnter.Add(fixLayout)
-
